23c2cfad6bd36262372233e1c34a02d2b15c91f1
Hugo Dupraz <hugo.dupraz@arcanite.ch>
2022-01-29 23:53:21 +0100

    fixes

--- Assets/Scripts/Mechanics/PhysicalEntity.cs
+++ Assets/Scripts/Mechanics/PhysicalEntity.cs
@@ -1,38 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Platformer.Gameplay;
+using Platformer.Core;
+using Platformer.Mechanics;
+using Platformer.Model;
 
-public class PhysicalEntity : MonoBehaviour
+using static Platformer.Core.Simulation;
+namespace Platformer.Mechanics
 {
-    public int team = 0;
-    public float hp = 10f;
-
-    public EnemyController enemy;
-
-    void Awake()
+    public class PhysicalEntity : MonoBehaviour
     {
-        if (team == 1) enemy = GetComponent<EnemyController>();
-    }
+        public int team = 0;
+        public float hp = 10f;
 
-    void Start()
-    {
-        
-    }
+        public EnemyController enemy;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (hp < 0f)
+        void Awake()
         {
-            if (team == 1) Schedule<EnemyDeath>().enemy = enemy;
-
-            if (team == 0) Schedule<PlayerDeath>();
+            if (team == 1) enemy = GetComponent<EnemyController>();
         }
-        
-    }
 
-    void DoDamage(float damage)
-    {
-        hp -= damage;
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (hp < 0f)
+            {
+                if (team == 1) Schedule<EnemyDeath>().enemy = enemy;
+
+                if (team == 0) Schedule<PlayerDeath>();
+            }
+            
+        }
+
+        public void DoDamage(float damage)
+        {
+            hp -= damage;
+        }
     }
 }
