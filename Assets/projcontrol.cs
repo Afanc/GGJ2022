cc96d4e803e4898a38bb5d98d3769b83b97fe45b
Hugo Dupraz <hugo.dupraz@arcanite.ch>
2022-01-30 17:57:44 +0100

    try aim fb

--- Assets/projcontrol.cs
+++ Assets/projcontrol.cs
@@ -2,18 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class projcontrol : MonoBehaviour
+namespace Platformer.Mechanics
 {
-    public Vector3 dv = new Vector3(-115.1f, 0.2f, 0);
-
-    // Start is called before the first frame update
-    void Start()
+    public class projcontrol : MonoBehaviour
     {
-    }
+        public Vector3 dv = new Vector3(-115.1f, 0.2f, 0);
 
-    // Update is called once per frame
-    void Update()
-    {
-       transform.position += dv * Time.deltaTime;
+        // Start is called before the first frame update
+        void Start()
+        {
+            int play_nbr = 2;
+            if (transform.position.y > -5f) play_nbr = 1;
+            
+            var players = GameObject.FindGameObjectsWithTag("Player");
+
+            foreach (GameObject player in players)
+            {
+                print(player.GetComponent<PlayerController>().PlayerNumber);
+            }
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+           transform.position += dv * Time.deltaTime;
+        }
     }
 }
